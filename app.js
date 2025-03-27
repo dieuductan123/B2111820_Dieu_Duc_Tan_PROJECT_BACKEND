209df56883c8292324d2460d4a85d0869f458f1f
tandieuduc <ductandieu456@gmail.com>
2025-03-27 12:55:46 +0700

    Cai dat xu ly loi cho ung dung

--- app.js
+++ app.js
@@ -10,4 +10,14 @@ app.get("/", (req, res) => {
   res.json({ message: "Welcome to Borrow Book Application" });
 });
 
+app.use((req, res, next) => {
+  return next(new ApiError(404, "404 not found"));
+});
+
+app.use((err, req, res, next) => {
+  return res.status(err.statusCode || 500).json({
+    message: err.message,
+  });
+});
+
 module.exports = app;
