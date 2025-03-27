1c9ace72f917a92e33f234a503f20612cd6f315e
tandieuduc <ductandieu456@gmail.com>
2025-03-27 13:00:28 +0700

    Cai dat thu vien mongodb va ket noi server ung dung voi csdl MongoDB

--- app/config/index.js
+++ app/config/index.js
@@ -2,6 +2,10 @@ const config = {
   app: {
     port: process.env.port || 3000,
   },
+  db: {
+    uri:
+      process.env.MONGODB_URI || "mongodb://127.0.0.1:27017/QuanLyMuonSach_DB",
+  },
 };
 
 module.exports = config;
--- server.js
+++ server.js
@@ -1,7 +1,20 @@
 const app = require("./app");
 const config = require("./app/config");
+const MongoDB = require("./app/utils/mongodb.util");
 
-const PORT = config.app.port;
-app.listen(PORT, () => {
-  console.log(`Server is listening on port ${PORT}.`);
-});
+async function startServer() {
+  try {
+    await MongoDB.connect(config.db.uri);
+    console.log("Connect to database successfully");
+
+    const PORT = config.app.port;
+    app.listen(PORT, () => {
+      console.log(`Server is runing on port ${PORT}`);
+    });
+  } catch (error) {
+    console.log("Error in connect to database ", error);
+    process.exit();
+  }
+}
+
+startServer();
